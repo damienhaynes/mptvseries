c1d9e26bf866b397a301afdfe4689975e9bd6ee4
inker <inker@92bfd266-0955-0410-bd60-67d1d2af653c>
2006-10-31 22:35:15 +0000

    missed msg handler

--- MP-TVSeries/LogWriter.cs
+++ MP-TVSeries/LogWriter.cs
@@ -14,7 +14,9 @@ namespace WindowPlugins.GUITVSeries
         private StreamWriter m_LogStream;
         private System.Windows.Forms.ListBox m_ListLog;
         private MediaPortal.Dialogs.GUIDialogProgress m_DlgProgress;
+        private delegate void newMsghandler(string input);
         private delegate int itemAddDel(string msg);
+        private event newMsghandler newMsg;
 
         public void AddNotifier(ref System.Windows.Forms.ListBox notifier)
         {
@@ -28,7 +30,7 @@ namespace WindowPlugins.GUITVSeries
         public LogWriter()
         {
             String logfile = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            
+
 #if TEST
             logfile += @"\MP-TVSeries.log";
 #else
@@ -37,51 +39,54 @@ namespace WindowPlugins.GUITVSeries
             logfile += @"\Log\MP-TVSeries.log";
 #endif
             this.m_filename = logfile;
+            newMsg += new newMsghandler(LogWriter_newMsg);
         }
 
-        void LogWriter_newMsg(object sender, string entry)
+        void LogWriter_newMsg(string entry)
         {
-            
-            if (this.m_ListLog != null)
-                {
-                    itemAddDel del = new itemAddDel(m_ListLog.Items.Add);
 
-                    m_ListLog.Invoke(del, entry);
-                    int nTopIndex = m_ListLog.Items.Count - m_ListLog.Height / m_ListLog.ItemHeight;
-                    if (nTopIndex < 0)
-                        nTopIndex = 0;
-                    m_ListLog.TopIndex = nTopIndex;
-                }
-                //if (this.m_DlgProgress != null)
-                //{
-                //    int lineSize = 50;
-                //    if (entry.Length >= lineSize)
-                //    {
-                //        int split = lineSize;
-                //        for (int index = lineSize - 1; index >= 0; index--)
-                //        {
-                //            if (entry[index] == ' ')
-                //            {
-                //                split = index;
-                //                break;
-                //            }
-                //        }
-                //        this.m_DlgProgress.SetLine(1, entry.Substring(0, split - 1));
-                //        this.m_DlgProgress.SetLine(2, entry.Substring(split + 1));
-                //    }
-                //    else
-                //    {
-                //        this.m_DlgProgress.SetLine(1, entry);
-                //        this.m_DlgProgress.SetLine(2, "");
-                //    }
-                //    this.m_DlgProgress.Progress();
-                //}
+            if (this.m_ListLog != null)
+            {
+                itemAddDel del = new itemAddDel(m_ListLog.Items.Add);
+
+                m_ListLog.Invoke(del, entry);
+                int nTopIndex = m_ListLog.Items.Count - m_ListLog.Height / m_ListLog.ItemHeight;
+                if (nTopIndex < 0)
+                    nTopIndex = 0;
+                m_ListLog.TopIndex = nTopIndex;
+            }
+            //if (this.m_DlgProgress != null)
+            //{
+            //    int lineSize = 50;
+            //    if (entry.Length >= lineSize)
+            //    {
+            //        int split = lineSize;
+            //        for (int index = lineSize - 1; index >= 0; index--)
+            //        {
+            //            if (entry[index] == ' ')
+            //            {
+            //                split = index;
+            //                break;
+            //            }
+            //        }
+            //        this.m_DlgProgress.SetLine(1, entry.Substring(0, split - 1));
+            //        this.m_DlgProgress.SetLine(2, entry.Substring(split + 1));
+            //    }
+            //    else
+            //    {
+            //        this.m_DlgProgress.SetLine(1, entry);
+            //        this.m_DlgProgress.SetLine(2, "");
+            //    }
+            //    this.m_DlgProgress.Progress();
+            //}
         }
 
         public void Write(String entry)
         {
             lock (typeof(LogWriter))
             {
+                
+
                 if (File.Exists(this.m_filename))
                     this.m_LogStream = File.AppendText(this.m_filename);
                 else
@@ -94,47 +99,6 @@ namespace WindowPlugins.GUITVSeries
                 this.m_LogStream.Dispose();
             }
                 
-        }
-    }
-
-    public class SafeInvokeHelper
-    {
-        static readonly ModuleBuilder builder;
-        static readonly AssemblyBuilder myAsmBuilder;
-        static readonly Hashtable methodLookup;
-
-        static SafeInvokeHelper()
-        {
-            AssemblyName name = new AssemblyName();
-            name.Name = "temp";
-            myAsmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.Run);
-            builder = myAsmBuilder.DefineDynamicModule("TempModule");
-            methodLookup = new Hashtable();
-        }
-
-        public static void Invoke(System.Windows.Forms.Control obj, string methodName, params object[] paramValues)
-        {
-            Delegate del = null;
-            string key = obj.GetType().Name + "." + methodName;
-            if (methodLookup.Contains(key))
-                del = (Delegate)methodLookup[key];
-            else
-            {
-                Type[] paramList = new Type[obj.GetType().GetMethod(methodName).GetParameters().Length];
-                int n = 0;
-                foreach (ParameterInfo pi in obj.GetType().GetMethod(methodName).GetParameters()) paramList[n++] = pi.ParameterType;
-                TypeBuilder typeB = builder.DefineType("Test" + methodName, TypeAttributes.Class | TypeAttributes.AutoLayout | TypeAttributes.Public | TypeAttributes.Sealed, typeof(MulticastDelegate), PackingSize.Unspecified);
-                ConstructorBuilder conB = typeB.DefineConstructor(MethodAttributes.HideBySig | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName, CallingConventions.Standard, new Type[] { typeof(object), typeof(IntPtr) });
-                conB.SetImplementationFlags(MethodImplAttributes.Runtime);
-                MethodBuilder mb = typeB.DefineMethod("Invoke", MethodAttributes.Public | MethodAttributes.Virtual | MethodAttributes.HideBySig, typeof(void), paramList);
-                mb.SetImplementationFlags(MethodImplAttributes.Runtime);
-                Type tp = typeB.CreateType();
-                MethodInfo mi = obj.GetType().GetMethod(methodName);
-                del = MulticastDelegate.CreateDelegate(tp, obj, methodName);
-                methodLookup.Add(key, del);
-            }
-
-            obj.Invoke(del, paramValues);
         }
     }
 }
