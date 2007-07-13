d73879bbdc81b60e2b5069171ea70e18b69a4a4c
inker <inker@92bfd266-0955-0410-bd60-67d1d2af653c>
2007-07-13 18:01:20 +0000

    Several Things for Fanart

--- MP-TVSeries/loadingDisplay.Designer.cs
+++ MP-TVSeries/loadingDisplay.Designer.cs
@@ -31,7 +31,6 @@ namespace WindowPlugins.GUITVSeries
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loadingDisplay));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.series = new System.Windows.Forms.Label();
             this.season = new System.Windows.Forms.Label();
             this.episodes = new System.Windows.Forms.Label();
@@ -42,7 +41,7 @@ namespace WindowPlugins.GUITVSeries
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(186, 36);
+            this.label1.Location = new System.Drawing.Point(216, 155);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(163, 13);
             this.label1.TabIndex = 0;
@@ -55,59 +54,55 @@ namespace WindowPlugins.GUITVSeries
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(158, 107);
+            this.pictureBox1.Size = new System.Drawing.Size(560, 186);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(186, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 16);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "MP-TV Series";
+            this.pictureBox1.UseWaitCursor = true;
             // 
             // series
             // 
             this.series.AutoSize = true;
-            this.series.Location = new System.Drawing.Point(186, 68);
+            this.series.BackColor = System.Drawing.Color.Transparent;
+            this.series.Location = new System.Drawing.Point(180, 172);
             this.series.Name = "series";
             this.series.Size = new System.Drawing.Size(35, 13);
             this.series.TabIndex = 3;
             this.series.Text = "label3";
+            this.series.UseWaitCursor = true;
             // 
             // season
             // 
             this.season.AutoSize = true;
-            this.season.Location = new System.Drawing.Point(186, 81);
+            this.season.BackColor = System.Drawing.Color.Transparent;
+            this.season.Location = new System.Drawing.Point(277, 172);
             this.season.Name = "season";
             this.season.Size = new System.Drawing.Size(35, 13);
             this.season.TabIndex = 4;
             this.season.Text = "label3";
+            this.season.UseWaitCursor = true;
             // 
             // episodes
             // 
             this.episodes.AutoSize = true;
-            this.episodes.Location = new System.Drawing.Point(186, 94);
+            this.episodes.BackColor = System.Drawing.Color.Transparent;
+            this.episodes.Location = new System.Drawing.Point(378, 172);
             this.episodes.Name = "episodes";
             this.episodes.Size = new System.Drawing.Size(35, 13);
             this.episodes.TabIndex = 5;
             this.episodes.Text = "label3";
+            this.episodes.UseWaitCursor = true;
             // 
             // loadingDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(419, 119);
+            this.ClientSize = new System.Drawing.Size(584, 194);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.episodes);
             this.Controls.Add(this.season);
             this.Controls.Add(this.series);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "loadingDisplay";
@@ -128,7 +123,6 @@ namespace WindowPlugins.GUITVSeries
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label series;
         private System.Windows.Forms.Label season;
         private System.Windows.Forms.Label episodes;
