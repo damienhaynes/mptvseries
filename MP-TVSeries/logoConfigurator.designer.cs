f20a0930b76a9328a3aacb73a37cd0fd34a36b89
damienh <damienh@92bfd266-0955-0410-bd60-67d1d2af653c>
2007-08-07 13:41:03 +0000

    - Added Tooltips to logo configuration dialog - Made default width slightly bigger - Added description to Folder selection dialog

--- MP-TVSeries/logoConfigurator.designer.cs
+++ MP-TVSeries/logoConfigurator.designer.cs
@@ -52,6 +52,8 @@ namespace WindowPlugins.GUITVSeries
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(logoConfigurator));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.browse = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -74,6 +76,7 @@ namespace WindowPlugins.GUITVSeries
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.btnPaste = new System.Windows.Forms.Button();
             this.btnCopy = new System.Windows.Forms.Button();
+            this.toolTipLogos = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -84,11 +87,12 @@ namespace WindowPlugins.GUITVSeries
             // 
             this.browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.browse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.browse.Location = new System.Drawing.Point(220, 23);
+            this.browse.Location = new System.Drawing.Point(245, 27);
             this.browse.Name = "browse";
             this.browse.Size = new System.Drawing.Size(75, 23);
             this.browse.TabIndex = 2;
             this.browse.Text = "&Browse...";
+            this.toolTipLogos.SetToolTip(this.browse, "Click to browse for a logo image to be used with this rule.");
             this.browse.UseVisualStyleBackColor = true;
             this.browse.Click += new System.EventHandler(this.browse_Click);
             // 
@@ -96,22 +100,28 @@ namespace WindowPlugins.GUITVSeries
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Location = new System.Drawing.Point(15, 29);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(202, 20);
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
             this.textBox1.TabIndex = 1;
+            this.toolTipLogos.SetToolTip(this.textBox1, "This contains the filename of the logo that the rule will load if condition is sa" +
+                    "tisfied.\r\nWhen a dynamic rule is applied, enter in the dynamic filename after th" +
+                    "e path chosen");
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // cond1_what
             // 
             this.cond1_what.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.cond1_what.Location = new System.Drawing.Point(72, 51);
+            this.cond1_what.Location = new System.Drawing.Point(75, 55);
+            this.cond1_what.Multiline = false;
             this.cond1_what.Name = "cond1_what";
-            this.cond1_what.Size = new System.Drawing.Size(142, 20);
+            this.cond1_what.Size = new System.Drawing.Size(164, 20);
             this.cond1_what.TabIndex = 4;
             this.cond1_what.Text = "";
+            this.toolTipLogos.SetToolTip(this.cond1_what, "Enter in the L.H.S of the expression to be evaluated.\r\nYou can right click on thi" +
+                    "s textbox to bring up a menu of available fields from the database");
             this.cond1_what.TextChanged += new System.EventHandler(this.cond1_what_TextChanged);
             // 
             // cond1_type
@@ -128,24 +138,26 @@ namespace WindowPlugins.GUITVSeries
             "<=",
             "contains",
             "!contains"});
-            this.cond1_type.Location = new System.Drawing.Point(220, 51);
+            this.cond1_type.Location = new System.Drawing.Point(245, 55);
             this.cond1_type.Name = "cond1_type";
             this.cond1_type.Size = new System.Drawing.Size(75, 21);
             this.cond1_type.TabIndex = 5;
+            this.toolTipLogos.SetToolTip(this.cond1_type, resources.GetString("cond1_type.ToolTip"));
             // 
             // cond1_cond
             // 
             this.cond1_cond.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.cond1_cond.Location = new System.Drawing.Point(301, 51);
+            this.cond1_cond.Location = new System.Drawing.Point(326, 55);
             this.cond1_cond.Name = "cond1_cond";
             this.cond1_cond.Size = new System.Drawing.Size(130, 20);
             this.cond1_cond.TabIndex = 6;
+            this.toolTipLogos.SetToolTip(this.cond1_cond, resources.GetString("cond1_cond.ToolTip"));
             this.cond1_cond.TextChanged += new System.EventHandler(this.cond1_cond_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 59);
+            this.label1.Location = new System.Drawing.Point(12, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(30, 13);
             this.label1.TabIndex = 7;
@@ -155,7 +167,7 @@ namespace WindowPlugins.GUITVSeries
             // 
             this.cond2_cond.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cond2_cond.Enabled = false;
-            this.cond2_cond.Location = new System.Drawing.Point(301, 78);
+            this.cond2_cond.Location = new System.Drawing.Point(326, 82);
             this.cond2_cond.Name = "cond2_cond";
             this.cond2_cond.Size = new System.Drawing.Size(130, 20);
             this.cond2_cond.TabIndex = 11;
@@ -176,7 +188,7 @@ namespace WindowPlugins.GUITVSeries
             "<=",
             "contains",
             "!contains"});
-            this.cond2_type.Location = new System.Drawing.Point(220, 78);
+            this.cond2_type.Location = new System.Drawing.Point(245, 82);
             this.cond2_type.Name = "cond2_type";
             this.cond2_type.Size = new System.Drawing.Size(75, 21);
             this.cond2_type.TabIndex = 10;
@@ -186,9 +198,10 @@ namespace WindowPlugins.GUITVSeries
             this.cond2_what.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.cond2_what.Enabled = false;
-            this.cond2_what.Location = new System.Drawing.Point(72, 78);
+            this.cond2_what.Location = new System.Drawing.Point(75, 82);
+            this.cond2_what.Multiline = false;
             this.cond2_what.Name = "cond2_what";
-            this.cond2_what.Size = new System.Drawing.Size(142, 20);
+            this.cond2_what.Size = new System.Drawing.Size(164, 20);
             this.cond2_what.TabIndex = 9;
             this.cond2_what.Text = "";
             this.cond2_what.TextChanged += new System.EventHandler(this.cond2_what_TextChanged);
@@ -201,10 +214,11 @@ namespace WindowPlugins.GUITVSeries
             this._12_link.Items.AddRange(new object[] {
             "AND",
             "OR"});
-            this._12_link.Location = new System.Drawing.Point(9, 78);
+            this._12_link.Location = new System.Drawing.Point(12, 82);
             this._12_link.Name = "_12_link";
             this._12_link.Size = new System.Drawing.Size(57, 21);
             this._12_link.TabIndex = 8;
+            this.toolTipLogos.SetToolTip(this._12_link, resources.GetString("_12_link.ToolTip"));
             // 
             // _23_link
             // 
@@ -214,7 +228,7 @@ namespace WindowPlugins.GUITVSeries
             this._23_link.Items.AddRange(new object[] {
             "AND",
             "OR"});
-            this._23_link.Location = new System.Drawing.Point(9, 104);
+            this._23_link.Location = new System.Drawing.Point(12, 108);
             this._23_link.Name = "_23_link";
             this._23_link.Size = new System.Drawing.Size(57, 21);
             this._23_link.TabIndex = 12;
@@ -223,7 +237,7 @@ namespace WindowPlugins.GUITVSeries
             // 
             this.cond3_cond.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cond3_cond.Enabled = false;
-            this.cond3_cond.Location = new System.Drawing.Point(301, 104);
+            this.cond3_cond.Location = new System.Drawing.Point(326, 108);
             this.cond3_cond.Name = "cond3_cond";
             this.cond3_cond.Size = new System.Drawing.Size(130, 20);
             this.cond3_cond.TabIndex = 15;
@@ -243,7 +257,7 @@ namespace WindowPlugins.GUITVSeries
             "<=",
             "contains",
             "!contains"});
-            this.cond3_type.Location = new System.Drawing.Point(220, 104);
+            this.cond3_type.Location = new System.Drawing.Point(245, 108);
             this.cond3_type.Name = "cond3_type";
             this.cond3_type.Size = new System.Drawing.Size(75, 21);
             this.cond3_type.TabIndex = 14;
@@ -253,9 +267,10 @@ namespace WindowPlugins.GUITVSeries
             this.cond3_what.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.cond3_what.Enabled = false;
-            this.cond3_what.Location = new System.Drawing.Point(72, 104);
+            this.cond3_what.Location = new System.Drawing.Point(75, 108);
+            this.cond3_what.Multiline = false;
             this.cond3_what.Name = "cond3_what";
-            this.cond3_what.Size = new System.Drawing.Size(142, 20);
+            this.cond3_what.Size = new System.Drawing.Size(164, 20);
             this.cond3_what.TabIndex = 13;
             this.cond3_what.Text = "";
             this.cond3_what.TextChanged += new System.EventHandler(this.cond3_what_TextChanged);
@@ -263,11 +278,13 @@ namespace WindowPlugins.GUITVSeries
             // save
             // 
             this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.save.Location = new System.Drawing.Point(356, 131);
+            this.save.Location = new System.Drawing.Point(381, 135);
             this.save.Name = "save";
             this.save.Size = new System.Drawing.Size(75, 23);
             this.save.TabIndex = 19;
             this.save.Text = "&Save";
+            this.toolTipLogos.SetToolTip(this.save, "Click to close this dialog and save the logo rule, you can also press the <ENTER>" +
+                    " key");
             this.save.UseVisualStyleBackColor = true;
             this.save.Click += new System.EventHandler(this.save_Click);
             // 
@@ -275,18 +292,20 @@ namespace WindowPlugins.GUITVSeries
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(275, 131);
+            this.btnCancel.Location = new System.Drawing.Point(300, 135);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 18;
             this.btnCancel.Text = "Ca&ncel";
+            this.toolTipLogos.SetToolTip(this.btnCancel, "Click to close this dialog without saving logo rule, you can also press the <ESC>" +
+                    " key");
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 5);
+            this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(133, 13);
             this.label2.TabIndex = 0;
@@ -295,35 +314,48 @@ namespace WindowPlugins.GUITVSeries
             // btnDynFilename
             // 
             this.btnDynFilename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDynFilename.Location = new System.Drawing.Point(301, 23);
+            this.btnDynFilename.Location = new System.Drawing.Point(326, 27);
             this.btnDynFilename.Name = "btnDynFilename";
             this.btnDynFilename.Size = new System.Drawing.Size(130, 23);
             this.btnDynFilename.TabIndex = 3;
             this.btnDynFilename.Text = "&Dynamic Filename....";
+            this.toolTipLogos.SetToolTip(this.btnDynFilename, resources.GetString("btnDynFilename.ToolTip"));
             this.btnDynFilename.UseVisualStyleBackColor = true;
             this.btnDynFilename.Click += new System.EventHandler(this.btnDynFilename_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Enter in a path that contains the dynamic files for this logo rule.";
             // 
             // btnPaste
             // 
             this.btnPaste.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnPaste.Location = new System.Drawing.Point(90, 130);
+            this.btnPaste.Location = new System.Drawing.Point(93, 134);
             this.btnPaste.Name = "btnPaste";
             this.btnPaste.Size = new System.Drawing.Size(75, 23);
             this.btnPaste.TabIndex = 17;
             this.btnPaste.Text = "&Paste";
+            this.toolTipLogos.SetToolTip(this.btnPaste, "Click to Paste contents of clipboard to create a logo rule");
             this.btnPaste.UseVisualStyleBackColor = true;
             this.btnPaste.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnCopy
             // 
             this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnCopy.Location = new System.Drawing.Point(9, 130);
+            this.btnCopy.Location = new System.Drawing.Point(12, 134);
             this.btnCopy.Name = "btnCopy";
             this.btnCopy.Size = new System.Drawing.Size(75, 23);
             this.btnCopy.TabIndex = 16;
             this.btnCopy.Text = "&Copy";
+            this.toolTipLogos.SetToolTip(this.btnCopy, "Click to Copy the Logo rule to the clipboard");
             this.btnCopy.UseVisualStyleBackColor = true;
             this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // toolTipLogos
+            // 
+            this.toolTipLogos.AutoPopDelay = 15000;
+            this.toolTipLogos.InitialDelay = 500;
+            this.toolTipLogos.ReshowDelay = 100;
             // 
             // logoConfigurator
             // 
@@ -331,7 +363,7 @@ namespace WindowPlugins.GUITVSeries
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(442, 161);
+            this.ClientSize = new System.Drawing.Size(472, 169);
             this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.btnPaste);
             this.Controls.Add(this.btnDynFilename);
@@ -387,5 +419,6 @@ namespace WindowPlugins.GUITVSeries
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button btnPaste;
         private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ToolTip toolTipLogos;
     }
 }
