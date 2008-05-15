4817e40f00c1dacf9fd80afe74bd6be6adfe4f3d
damienh <damienh@92bfd266-0955-0410-bd60-67d1d2af653c>
2008-05-15 04:45:25 +0000

    * Updated versioning v2.0.*

--- MP-TVSeries/Properties/AssemblyInfo.cs
+++ MP-TVSeries/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MP-TVSeries")]
-[assembly: AssemblyDescription("Build 2.0.0")]
+[assembly: AssemblyDescription("Build 2.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MP-TVSeries")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright © 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+//[assembly: AssemblyFileVersion("2.0.0.0")]
