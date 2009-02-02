674d8d812dabc130e9a3f7e04c809546cced5dec
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2009-02-02 19:48:50 +0000

    * Updated trunk version to v2.2

--- MP-TVSeries/Properties/AssemblyInfo.cs
+++ MP-TVSeries/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MP-TVSeries")]
-[assembly: AssemblyDescription("Build 2.1")]
+[assembly: AssemblyDescription("Build 2.2")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MP-TVSeries")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.3321.0")]
+[assembly: AssemblyVersion("2.2.*")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
