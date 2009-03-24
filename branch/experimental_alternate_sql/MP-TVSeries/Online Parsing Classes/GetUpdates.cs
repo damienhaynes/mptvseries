822bd5fd46af863f4bc44a66a69e14981ab062d9
mmalham <mmalham@92bfd266-0955-0410-bd60-67d1d2af653c>
2009-03-24 09:09:49 +0000

    Created an experimental branch using System.Data.SQLite that can use either a SQLite database, or a MS SQL2005(2008) database

---
+++ branch/experimental_alternate_sql/MP-TVSeries/Online Parsing Classes/GetUpdates.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace WindowPlugins.GUITVSeries.Online_Parsing_Classes
+{
+    class GetUpdates
+    {
+        long timestamp;
+        List<DBValue> series = null;
+        List<DBValue> episodes = null;
+
+        public long OnlineTimeStamp
+        { get { return timestamp; } }
+
+        public List<DBValue> UpdatedSeries
+        { get { return series; } }
+
+        public List<DBValue> UpdatedEpisodes
+        { get { return episodes; } }
+
+        public GetUpdates(OnlineAPI.UpdateType type)
+        {
+            if (type != OnlineAPI.UpdateType.all)
+                MPTVSeriesLog.Write(string.Format("Downloading updates from the last {0}", type.ToString()));
+            else
+                MPTVSeriesLog.Write("Downloading all updates");
+
+            XmlNode updates = OnlineAPI.Updates(type);
+            if (updates == null)
+                return;
+       
+            long.TryParse(updates.Attributes["time"].Value, out this.timestamp);
+            
+            XmlNodeList nodelist = updates.ChildNodes;
+            
+            series = new List<DBValue>();
+            episodes = new List<DBValue>();
+            if (nodelist != null)
+            {
+                foreach (XmlNode node in nodelist)
+                {
+                    string entryType = node.Name;
+                    string id = string.Empty;
+                    for (int i = 0; i < node.ChildNodes.Count; i++)
+                    {
+                        if (node.ChildNodes[i].Name.Equals("id", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            id = node.ChildNodes[i].InnerText;
+                            break;
+                        }
+                    }
+
+                    if (id.Length > 0)
+                    {
+                        switch (entryType)
+                        {
+                            case "Episode":
+                                this.episodes.Add(id);
+                                break;
+                            case "Series":
+                                this.series.Add(id);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
