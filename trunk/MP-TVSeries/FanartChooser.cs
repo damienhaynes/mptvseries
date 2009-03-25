5303b68f078084e9a1a060b4debe0251f7e0be0b
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2009-03-25 05:27:04 +0000

    * Delete invalid fanart from disk if image can't be loaded (in Fanart Chooser window)

--- trunk/MP-TVSeries/FanartChooser.cs
+++ trunk/MP-TVSeries/FanartChooser.cs
@@ -879,10 +879,27 @@ namespace WindowPlugins.GUITVSeries
 
             TVSeriesPlugin.setGUIProperty("FanArt.SelectedFanartInfo", fanartInfo);
 
-            string preview = fanart.isAvailableLocally ?
-                             ImageAllocator.GetOtherImage(fanart.FullLocalPath, default(System.Drawing.Size), false) :
-                             m_Facade.SelectedListItem.IconImageBig;
-          
+            string preview = string.Empty;
+            
+            if (fanart.isAvailableLocally)
+            {
+                // Ensure Fanart on Disk is valid as well
+                if (ImageAllocator.LoadImageFastFromFile(fanart.FullLocalPath) == null)
+                {
+                    MPTVSeriesLog.Write("Fanart is invalid, deleting...");
+                    fanart.Delete();
+                    fanart.Chosen = false;
+                    m_Facade.SelectedListItem.Label = Translation.FanArtOnline;
+                }                    
+                
+                // Should be safe to assign size fanart if available
+                preview = fanart.isAvailableLocally ?
+                          ImageAllocator.GetOtherImage(fanart.FullLocalPath, default(System.Drawing.Size), false) :
+                          m_Facade.SelectedListItem.IconImageBig;
+            }
+            else
+                preview = m_Facade.SelectedListItem.IconImageBig;
+                      
             TVSeriesPlugin.setGUIProperty("FanArt.SelectedPreview", preview);
         }
     }
