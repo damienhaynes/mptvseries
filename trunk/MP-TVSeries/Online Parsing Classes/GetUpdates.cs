0612bd60e46cc0611041c8e22859e4d1113d1279
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2009-04-02 12:51:55 +0000

    * Change banner updates such that it only gets *new* artwork for series (previously was trying to get them if series meta data had changed)

--- trunk/MP-TVSeries/Online Parsing Classes/GetUpdates.cs
+++ trunk/MP-TVSeries/Online Parsing Classes/GetUpdates.cs
@@ -10,6 +10,7 @@ namespace WindowPlugins.GUITVSeries.Online_Parsing_Classes
         long timestamp;
         List<DBValue> series = null;
         List<DBValue> episodes = null;
+        List<DBValue> banners = null;
 
         public long OnlineTimeStamp
         { get { return timestamp; } }
@@ -19,6 +20,9 @@ namespace WindowPlugins.GUITVSeries.Online_Parsing_Classes
 
         public List<DBValue> UpdatedEpisodes
         { get { return episodes; } }
+
+        public List<DBValue> UpdatedBanners
+        { get { return banners; } }
 
         public GetUpdates(OnlineAPI.UpdateType type)
         {
@@ -32,40 +36,43 @@ namespace WindowPlugins.GUITVSeries.Online_Parsing_Classes
                 return;
        
             long.TryParse(updates.Attributes["time"].Value, out this.timestamp);
-            
-            XmlNodeList nodelist = updates.ChildNodes;
-            
-            series = new List<DBValue>();
-            episodes = new List<DBValue>();
-            if (nodelist != null)
-            {
-                foreach (XmlNode node in nodelist)
-                {
-                    string entryType = node.Name;
-                    string id = string.Empty;
-                    for (int i = 0; i < node.ChildNodes.Count; i++)
-                    {
-                        if (node.ChildNodes[i].Name.Equals("id", StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            id = node.ChildNodes[i].InnerText;
-                            break;
-                        }
-                    }
 
-                    if (id.Length > 0)
-                    {
-                        switch (entryType)
-                        {
-                            case "Episode":
-                                this.episodes.Add(id);
-                                break;
-                            case "Series":
-                                this.series.Add(id);
-                                break;
-                        }
-                    }
-                }
+            //get all the series ids
+            series = new List<DBValue>();
+            foreach (XmlNode node in updates.SelectNodes("/Data/Series/id")) {
+                this.series.Add(node.Value);
             }
+
+            //get all the episode ids
+            episodes = new List<DBValue>();
+            foreach (XmlNode node in updates.SelectNodes("/Data/Episode/id")) {
+                this.episodes.Add(node.Value);
+            }
+            
+            //get all the season banners
+            banners = new List<DBValue>();
+            string id = string.Empty;
+
+            foreach (XmlNode node in updates.SelectNodes("/Data/Banner[type='season']")) {
+                id = node.SelectSingleNode("Series").InnerText;
+                if (!this.banners.Contains(id))
+                    this.banners.Add(id);
+            }
+
+            //get all the series banners
+            foreach (XmlNode node in updates.SelectNodes("/Data/Banner[type='series']")) {
+                id = node.SelectSingleNode("Series").InnerText;
+                if (!this.banners.Contains(id))
+                    this.banners.Add(id);
+            }
+
+            //get all the poster banners
+            foreach (XmlNode node in updates.SelectNodes("/Data/Banner[type='poster']")) {
+                id = node.SelectSingleNode("Series").InnerText;
+                if (!this.banners.Contains(id))
+                    this.banners.Add(id);
+            }
+
         }
     }
 }
