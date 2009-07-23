07b3c3b17eee63f333af8ef2c29d8d6aca8fbc08
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2009-07-23 03:51:24 +0000

    Created Tag for v2.3.3491 release

---
+++ tags/v2.3.3491/MP-TVSeries/Configuration/PlaylistSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowPlugins.GUITVSeries.Configuration {
+	public partial class PlaylistSettings : UserControl {
+
+		private string PlaylistPath { get; set; }			
+		
+		public PlaylistSettings() {
+			InitializeComponent();
+			LoadFromDB();
+		}
+
+		private void LoadFromDB() {
+			// Playlist Folder
+			PlaylistPath = DBOption.GetOptions(DBOption.cPlaylistPath);
+			textBoxPlaylistFolder.Text = PlaylistPath;
+		}		
+
+		private void textBoxPlaylistFolder_TextChanged(object sender, EventArgs e) {			
+			DBOption.SetOptions(DBOption.cPlaylistPath, textBoxPlaylistFolder.Text);
+		}
+
+		private void textBoxBrowse_Click(object sender, EventArgs e) {
+			FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+			folderDialog.SelectedPath = PlaylistPath;
+			folderDialog.Description = "Select or create a New Path save/load tvseries playlists:";
+			DialogResult result = folderDialog.ShowDialog();
+			if (result == DialogResult.OK)
+				textBoxPlaylistFolder.Text = folderDialog.SelectedPath;            
+		}
+
+	}
+}
