0c6550221952d0c5013b94327ae6d302397ff064
StinkerInker <StinkerInker@92bfd266-0955-0410-bd60-67d1d2af653c>
2010-02-13 23:42:18 +0000

    First Revision of Import Wizard, needs polishing

---
+++ trunk/MP-TVSeries/ImportWizard/Configuration/PlaylistSettings.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowPlugins.GUITVSeries.Configuration {
+	partial class PlaylistSettings {
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBoxBrowse = new System.Windows.Forms.Button();
+			this.textBoxPlaylistFolder = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dbOptCheckBoxAutoShuffle = new WindowPlugins.GUITVSeries.Configuration.DBOptionCheckBox();
+			this.dbOptCheckBoxAutoPlayList = new WindowPlugins.GUITVSeries.Configuration.DBOptionCheckBox();
+			this.dbOptCheckBoxRepeatPlaylist = new WindowPlugins.GUITVSeries.Configuration.DBOptionCheckBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.dbOptCheckBoxAutoShuffle);
+			this.groupBox1.Controls.Add(this.dbOptCheckBoxAutoPlayList);
+			this.groupBox1.Controls.Add(this.dbOptCheckBoxRepeatPlaylist);
+			this.groupBox1.Controls.Add(this.textBoxBrowse);
+			this.groupBox1.Controls.Add(this.textBoxPlaylistFolder);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(4, 0);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(537, 143);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Playlist View";
+			// 
+			// textBoxBrowse
+			// 
+			this.textBoxBrowse.Location = new System.Drawing.Point(398, 28);
+			this.textBoxBrowse.Name = "textBoxBrowse";
+			this.textBoxBrowse.Size = new System.Drawing.Size(75, 23);
+			this.textBoxBrowse.TabIndex = 2;
+			this.textBoxBrowse.Text = "&Browse...";
+			this.textBoxBrowse.UseVisualStyleBackColor = true;
+			this.textBoxBrowse.Click += new System.EventHandler(this.textBoxBrowse_Click);
+			// 
+			// textBoxPlaylistFolder
+			// 
+			this.textBoxPlaylistFolder.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+			this.textBoxPlaylistFolder.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystemDirectories;
+			this.textBoxPlaylistFolder.Location = new System.Drawing.Point(109, 30);
+			this.textBoxPlaylistFolder.Name = "textBoxPlaylistFolder";
+			this.textBoxPlaylistFolder.Size = new System.Drawing.Size(283, 20);
+			this.textBoxPlaylistFolder.TabIndex = 1;
+			this.textBoxPlaylistFolder.TextChanged += new System.EventHandler(this.textBoxPlaylistFolder_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(15, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Playlist &Folder:";
+			// 
+			// dbOptCheckBoxAutoShuffle
+			// 
+			this.dbOptCheckBoxAutoShuffle.AutoSize = true;
+			this.dbOptCheckBoxAutoShuffle.Location = new System.Drawing.Point(109, 114);
+			this.dbOptCheckBoxAutoShuffle.Name = "dbOptCheckBoxAutoShuffle";
+			this.dbOptCheckBoxAutoShuffle.Option = "PlaylistAutoShuffle";
+			this.dbOptCheckBoxAutoShuffle.Size = new System.Drawing.Size(180, 17);
+			this.dbOptCheckBoxAutoShuffle.TabIndex = 5;
+			this.dbOptCheckBoxAutoShuffle.Text = "Auto &shuffle playlist when loaded";
+			this.dbOptCheckBoxAutoShuffle.ToolTip = "";
+			this.dbOptCheckBoxAutoShuffle.UseVisualStyleBackColor = true;
+			// 
+			// dbOptCheckBoxAutoPlayList
+			// 
+			this.dbOptCheckBoxAutoPlayList.AutoSize = true;
+			this.dbOptCheckBoxAutoPlayList.Checked = true;
+			this.dbOptCheckBoxAutoPlayList.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.dbOptCheckBoxAutoPlayList.Location = new System.Drawing.Point(109, 90);
+			this.dbOptCheckBoxAutoPlayList.Name = "dbOptCheckBoxAutoPlayList";
+			this.dbOptCheckBoxAutoPlayList.Option = "PlaylistAutoPlay";
+			this.dbOptCheckBoxAutoPlayList.Size = new System.Drawing.Size(168, 17);
+			this.dbOptCheckBoxAutoPlayList.TabIndex = 4;
+			this.dbOptCheckBoxAutoPlayList.Text = "A&uto play playlist when loaded";
+			this.dbOptCheckBoxAutoPlayList.ToolTip = "";
+			this.dbOptCheckBoxAutoPlayList.UseVisualStyleBackColor = true;
+			// 
+			// dbOptCheckBoxRepeatPlaylist
+			// 
+			this.dbOptCheckBoxRepeatPlaylist.AutoSize = true;
+			this.dbOptCheckBoxRepeatPlaylist.Location = new System.Drawing.Point(109, 66);
+			this.dbOptCheckBoxRepeatPlaylist.Name = "dbOptCheckBoxRepeatPlaylist";
+			this.dbOptCheckBoxRepeatPlaylist.Option = "RepeatPlaylist";
+			this.dbOptCheckBoxRepeatPlaylist.Size = new System.Drawing.Size(180, 17);
+			this.dbOptCheckBoxRepeatPlaylist.TabIndex = 3;
+			this.dbOptCheckBoxRepeatPlaylist.Text = "Repeat/&Loop episodes in playlist";
+			this.dbOptCheckBoxRepeatPlaylist.ToolTip = "";
+			this.dbOptCheckBoxRepeatPlaylist.UseVisualStyleBackColor = true;
+			// 
+			// PlaylistSettings
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.groupBox1);
+			this.Name = "PlaylistSettings";
+			this.Size = new System.Drawing.Size(544, 155);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button textBoxBrowse;
+		private System.Windows.Forms.TextBox textBoxPlaylistFolder;
+		private System.Windows.Forms.Label label1;
+		private DBOptionCheckBox dbOptCheckBoxRepeatPlaylist;
+		private DBOptionCheckBox dbOptCheckBoxAutoPlayList;
+		private DBOptionCheckBox dbOptCheckBoxAutoShuffle;
+	}
+}
---
+++ trunk/MP-TVSeries/ImportWizard/LogWriter.cs
@@ -0,0 +1,257 @@
+﻿#region GNU license
+// MP-TVSeries - Plugin for Mediaportal
+// http://www.team-mediaportal.com
+// Copyright (C) 2006-2007
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#endregion
+
+using System;
+using System.IO;
+using System.Threading;
+
+namespace WindowPlugins.GUITVSeries
+{
+    public class MPTVSeriesLog // can't call it Log because MP's own Log is called that
+    {
+        const bool OmmitKey = true;
+        public enum LogLevel
+        {
+            Normal,
+            Debug,
+            DebugSQL
+        }
+
+        #region Vars
+        static private LogLevel _selectedLogLevel = LogLevel.Normal;
+        static String m_filename = Settings.GetPath(Settings.Path.log);
+        static StreamWriter m_LogStream;
+        static System.Windows.Forms.ListBox m_ListLog;
+        static MediaPortal.Dialogs.GUIDialogProgress m_DlgProgress;
+        delegate void Log_WriteCallback(string input);
+        #endregion
+
+        #region Public Fields
+        public static bool pauseAutoWriteDB = false;
+        #endregion
+
+        #region Properties
+        public static LogLevel selectedLogLevel
+	    {
+		    get { return _selectedLogLevel;}
+		    set {
+                    if (value != _selectedLogLevel)
+                    {
+                        Write("Switched LogLevel to: " + value.ToString());
+                        _selectedLogLevel = value;
+                        if (!pauseAutoWriteDB)
+                            DBOption.SetOptions("logLevel", (int)value);
+                    }
+                }
+            }
+        #endregion
+
+        #region Constructor
+        static MPTVSeriesLog()
+        {
+            if (System.Diagnostics.Process.GetCurrentProcess().ProcessName.ToLower() == "devenv")
+                return;
+ 
+            // let's rename the old one to .bak just like MP does
+            try
+            {
+                if (File.Exists(m_filename))
+                {
+                    string bgFile = Settings.GetPath(Settings.Path.logBackup);
+                    if (File.Exists(bgFile)) File.Delete(bgFile);
+                    File.Move(m_filename, Settings.GetPath(Settings.Path.logBackup));
+                }
+            }
+            catch (Exception e)
+            {
+                Write("Problem backing up Log file: " + e.Message);
+            }
+            try
+            {
+                m_LogStream = File.CreateText(m_filename);
+                m_LogStream.Close();
+                m_LogStream.Dispose();
+            }
+            catch (Exception)
+            {
+                // oopps, can't create file
+            }
+
+            selectedLogLevel = LogLevel.Normal;
+            pauseAutoWriteDB = true;
+            
+        }
+        #endregion
+
+        #region Public Config Methods
+        static public void AddNotifier(ref System.Windows.Forms.ListBox notifier)
+        {
+            m_ListLog = notifier;
+        }
+        static public void AddNotifier(ref MediaPortal.Dialogs.GUIDialogProgress notifier)
+        {
+            m_DlgProgress = notifier;
+        }
+        #endregion
+
+        #region Public Write Methods
+        /// <summary>
+        /// Use this for Std. Log entries, only show up in LogLevel.Normal
+        /// </summary>
+        /// <param name="entry"></param>
+        static public void Write(String entry)
+        {
+            Write(entry, LogLevel.Normal);
+        }
+
+        /// <summary>
+        /// Use this for Log entries with many lines (e.g. StackTrace), only show up in LogLevel.Normal
+        /// </summary>
+        /// <param name="entry"></param>
+        static public void WriteMultiLine(String entry, LogLevel level)
+        {
+            Write(entry, level, false);
+        }
+
+        /// <summary>
+        /// Use this for Log entries with many lines (e.g. StackTrace)
+        /// </summary>
+        /// <param name="entry"></param>
+        static public void WriteMultiLine(String entry)
+        {
+            WriteMultiLine(entry, LogLevel.Normal);
+        }
+
+        /// <summary>
+        /// To avoid having to join values if not needed in lower LogLevels use this
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <param name="param"></param>
+        /// <param name="level"></param>
+        static public void Write(String entry, int param, LogLevel level)
+        {
+            // speed things up so for 2 strings they will only get joined if really needed, 
+            // otherwise all those debug calls will be slog
+            if ((int)level <= (int)selectedLogLevel)
+            {
+                Write(entry + param.ToString(), level);
+            }
+        }
+
+        /// <summary>
+        /// To avoid having to join values if not needed in lower LogLevels use this
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <param name="param"></param>
+        /// <param name="level"></param>
+        static public void Write(String entry, String param, LogLevel level)
+        {
+            // speed things up so for 2 strings they will only get joined if really needed, 
+            // otherwise all those debug calls will be slog
+            if ((int)level <= (int)selectedLogLevel)
+            {
+                Write(entry + param, level);
+            }
+        }
+
+        /// <summary>
+        /// Use this for Std. Log entries, only show up in LogLevel.Normal
+        /// </summary>
+        /// <param name="entry"></param>
+        static public void Write(String entry, LogLevel level)
+        {
+            Write(entry, level, true);
+        }
+        #endregion
+
+        #region Implementation
+        static void Write(string entry, LogLevel level, bool singleLine)
+        {
+            if ((int)level <= (int)selectedLogLevel)
+            {
+                if (m_LogStream != null)
+                {
+                    if (OmmitKey && !String.IsNullOrEmpty(DBOnlineMirror.cApiKey))
+                        entry = entry.Replace(DBOnlineMirror.cApiKey, "<apikey>");
+
+                    String sPrefix = String.Format("{0:D8} - {1} - ", Thread.CurrentThread.ManagedThreadId, DateTime.Now);
+
+                    // note: keep the lock block as small as possible
+                    // don't call anything from within that could potentially call for a logwrite itself, or we have a deadlock
+                    lock (m_LogStream) 
+                    {
+                        try
+                        {
+                            if (File.Exists(m_filename))
+                                m_LogStream = File.AppendText(m_filename);
+                            else
+                                m_LogStream = File.CreateText(m_filename);
+                                                        
+                            if (singleLine)
+                                m_LogStream.WriteLine(sPrefix + entry);
+                            else
+                                m_LogStream.Write(sPrefix + "\n" + entry);
+
+                            m_LogStream.Flush();
+                            m_LogStream.Close();
+                            m_LogStream.Dispose();
+
+                        }
+                        catch (Exception ex)
+                        {
+                            // well we can't write...maybe no file access or something....and we can't even log the error
+                            Log_Write(entry);
+                            Log_Write(ex.Message);                           
+                            //m_LogStream.Close();
+                            //m_LogStream.Dispose(); //Crash
+                        }
+                    }
+                }
+                // Dont log debug msgs to Configuration Logger
+                if (level != LogLevel.Debug)
+                    Log_Write(entry);
+            }
+        }
+
+        static void Log_Write(String entry)
+        {
+            if (m_ListLog != null)
+            {
+                if (m_ListLog.InvokeRequired)
+                {
+                    Log_WriteCallback d = new Log_WriteCallback(Log_Write);
+                    m_ListLog.Invoke(d, new object[] { entry });
+                }
+                else
+                {
+                    m_ListLog.Items.Add(DateTime.Now + " - " + entry);
+                    int nTopIndex = m_ListLog.Items.Count - m_ListLog.Height / m_ListLog.ItemHeight;
+                    if (nTopIndex < 0)
+                        nTopIndex = 0;
+                    m_ListLog.TopIndex = nTopIndex;
+                }
+            }
+        }
+        #endregion
+    }
+}
