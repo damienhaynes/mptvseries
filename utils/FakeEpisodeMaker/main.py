eda61ef53ed89b387d558be77f874d516bd71bb3
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2010-07-08 07:21:30 +0000

    Modified python fake episode creator to also include episode titles in file names.

--- utils/FakeEpisodeMaker/main.py
+++ utils/FakeEpisodeMaker/main.py
@@ -3,31 +3,53 @@ import os
 import openAnything
 from xml.dom import minidom
 
+def replace_all(text, dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
+
 def parseShow(seriesID, show_name):
-    safe_show_name = show_name.replace(":", "")
+    reps =  {'\\':'-', '/':'-', ':':'', '?':'', '"':'\'', '*':'', '<':'', '>':'', '|':'', 'U+201C':'\'', 'U+201D':'', '\t':''}
+    safe_show_name = replace_all(show_name, reps)
     details_url = "http://thetvdb.com/api/EB49E8B9E78EBEE1/series/"+seriesID+"/all/en.xml"
     details = openAnything.fetch(details_url)
     details_xml = minidom.parseString(details['data'])
+    epnames = details_xml.getElementsByTagName("EpisodeName")
     seasons = details_xml.getElementsByTagName("SeasonNumber")
-    episodes = details_xml.getElementsByTagName("EpisodeNumber")
+    episodes = details_xml.getElementsByTagName("EpisodeNumber")    
     # check to see if parent show path needs to be made
     if not os.access(safe_show_name, os.F_OK):
         os.makedirs(safe_show_name)
     i = 0
     for item in episodes:
         season = seasons[i].firstChild.data
-        episode = item.firstChild.data
-        filename = safe_show_name+" S"+season.zfill(2)+"E"+episode.zfill(2)+".avi"
+        episode = item.firstChild.data        
+        try:
+			epname = epnames[i].firstChild.data
+        except:
+            epname = "Season "+season+" Episode "+episode
+        safe_episode_name = replace_all(epname, reps)
+        filename = safe_show_name+" - S"+season.zfill(2)+"E"+episode.zfill(2)+" - "+safe_episode_name        
+        path = safe_show_name + "\\Season " + season
         # seeif season path exists or not, and make it if not
-        if os.access(safe_show_name + "\\Season " + season, os.F_OK):
+        if os.access(path, os.F_OK):
+            # path/filename may exceed 255 chars
+            fullpath = path + "\\" + filename
+            safepath = fullpath[0:200]+".avi"
             # just go ahead and create the file
-            file = open(safe_show_name + "\\Season " + season + "\\" + filename, "w")
+            file = open(safepath, "w")
             file.close()
         else:
-            os.makedirs(safe_show_name + "\\Season " + season)
-            file = open(safe_show_name + "\\Season " + season + "\\" + filename, "w")
+            os.makedirs(path)
+            # path/filename may exceed 255 chars
+            fullpath = path + "\\" + filename
+            safepath = fullpath[0:200]+".avi"
+            file = open(safepath, "w")
             file.close()
-        print "Creating %s" % filename
+        try:
+            print "Creating %s" % safepath
+        except:
+            print "Creating %s" % "Season "+season+" Episode "+episode
         i = i + 1
         
 show_file = open("shows.txt")
