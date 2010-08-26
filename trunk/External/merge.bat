4586feff334a93b23d8b947e51226f096d6d65d2
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2010-08-26 04:16:12 +0000

    Cleanup Build Environment:  * Removed postbuild copying and replaced with custom postbuild.bat if it exists. Developers can just create the PostBuild.bat in their ProjectDir and create a custom post build environment.  * Added MediaPortal libraries to Externals so we have relative paths to references that suit everybody.
    New Developers should just be able to do a Checkout and it should just work.

--- trunk/External/merge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-IF EXIST MP-TVSeries_UNMERGED.dll del MP-TVSeries_UNMERGED.dll
-ren MP-TVSeries.dll MP-TVSeries_UNMERGED.dll
-ilmerge /out:MP-TVSeries.dll MP-TVSeries_UNMERGED.dll ICSharpCode.SharpZipLib.dll
