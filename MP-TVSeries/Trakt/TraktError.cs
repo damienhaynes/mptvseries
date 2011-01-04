860fc8b6e4a1b16990c5416934f7735b92eef59b
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2011-01-04 01:01:30 +0000

    Added ability to get a users profile from trakt. Isn't hooked up to anything yet. Also added Trakt Error status if any to the corresponding object.

---
+++ MP-TVSeries/Trakt/TraktError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace WindowPlugins.GUITVSeries.Trakt
+{
+    [DataContract]
+    public class TraktError
+    {
+        [DataMember]
+        public string status { get; set;}
+
+        [DataMember]
+        public string message { get; set; }
+    }
+}
