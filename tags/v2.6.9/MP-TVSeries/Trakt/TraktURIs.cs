ac26f13469861721f11168b179c5b84a43017ee5
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2011-01-23 08:35:14 +0000

    Created tag of release 2.6.9.1369.

---
+++ tags/v2.6.9/MP-TVSeries/Trakt/TraktURIs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trakt
+{
+    public static class TraktURIs
+    {
+        public const string ScrobbleShow = @"http://api.trakt.tv/show/{0}/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4";
+        public const string ScrobbleMovie = @"http://api.trakt.tv/movie/{0}/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4";
+        public const string UserWatchedEpisodes = @"http://api.trakt.tv/user/watched/episodes.json/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4/{0}";
+        public const string UserWatchedMovies = @"http://api.trakt.tv/user/watched/movies.json/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4/{0}";
+        public const string UserProfile = @"http://api.trakt.tv/user/profile.json/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4/{0}";
+        public const string SeriesOverview = @"http://api.trakt.tv/show/summary.json/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4/{0}";
+        public const string UserLibraryShows = @"http://api.trakt.tv/user/library/shows.json/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4/{0}";
+        public const string UserLibraryMovies = @"http://api.trakt.tv/user/library/movies.json/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4/{0}";
+        public const string SyncEpisodeLibrary = @"http://api.trakt.tv/show/episode/{0}/5daf4d0b339a90d7473c6f1ed7f609c4e69f92b4";        
+    }
+}
---
+++ tags/v2.6.9/MP-TVSeries/Trakt/User/TraktWatchedEpisodeHistory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Trakt.User
+{
+    [DataContract]
+    public class TraktWatchedEpisodeHistory : TraktResponse
+    {
+        [DataMember(Name="watched")]
+        public string WatchedID { get; set; }
+
+        [DataMember(Name="show")]
+        public Series Show { get; set; }
+
+        [DataMember(Name="episode")]
+        public Episode Episode { get; set; }     
+    }
+
+    [DataContract]
+    public class Series
+    {
+        [DataMember(Name="title")]
+        public string Title { get; set; }
+
+        [DataMember(Name="url")]
+        public string Url { get; set; }
+
+        [DataMember(Name="tvdb_id")]
+        public string SeriesID { get; set; }
+    }
+
+    [DataContract]
+    public class Episode
+    {
+        [DataMember(Name="season")]
+        public string SeasonIndex { get; set; }
+
+        [DataMember(Name="number")]
+        public string EpisodeIndex { get; set; }
+
+        [DataMember(Name="title")]
+        public string Title { get; set; }
+
+        [DataMember(Name="url")]
+        public string Url { get; set; }
+        
+        [DataMember(Name="first_aired")]
+        public string FirstAired { get; set; }
+    }
+}
