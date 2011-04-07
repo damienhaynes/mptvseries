33beb1b6607a5dc71302d723a2a02ca6e72000f1
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2011-04-07 08:42:31 +0000

    Fix web browser not working in installer for some newer browsers (e.g. FF4).

---
+++ branch/MP11/MSI/MP-TVSeries/Dll/Setup/Setup/Web.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Setup
+{
+    public class Web
+    {
+        public void LaunchBrowser(string url)
+        {
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}
