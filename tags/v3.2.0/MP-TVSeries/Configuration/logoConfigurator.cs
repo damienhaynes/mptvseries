ccae6cb24c00ed4b11fe7012b0f5ccccdb207f9c
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2012-01-19 05:29:31 +0000

    Created tag for v3.2.0 release.

---
+++ tags/v3.2.0/MP-TVSeries/Configuration/logoConfigurator.cs
@@ -0,0 +1,312 @@
+#region GNU license
+// MP-TVSeries - Plugin for Mediaportal
+// http://www.team-mediaportal.com
+// Copyright (C) 2006-2007
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#endregion
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowPlugins.GUITVSeries
+{
+    public partial class logoConfigurator : Form
+    {
+        public delegate void validDelegate(ref RichTextBox txtBox);
+        validDelegate validateTxtBox;
+        string appPath = Settings.GetPath(Settings.Path.app);
+        public logoConfigurator(validDelegate validDel, ContextMenuStrip strip)
+        {
+            init(validDel, strip);
+        }
+
+        public logoConfigurator(validDelegate validDel, ContextMenuStrip strip, string entryToEdit)
+        {
+            init(validDel, strip);
+            parseForEdit(entryToEdit);
+        }
+
+        public void init(validDelegate validDel, ContextMenuStrip strip)
+        {
+            InitializeComponent();
+            validateTxtBox = validDel;
+            FieldTag forWhats = new FieldTag("lastField", FieldTag.Level.Episode);
+            forWhats.m_bInited = true; // avoid autofilling
+            this.cond1_what.Tag = forWhats;
+            this.cond1_what.ContextMenuStrip = strip;
+            this.cond2_what.Tag = forWhats;
+            this.cond3_what.Tag = forWhats;
+
+            this.textBox1.Tag = forWhats;
+
+            this.cond1_type.SelectedIndex = 0;
+            this.cond2_type.SelectedIndex = 0;
+            this.cond3_type.SelectedIndex = 0;
+
+            this._12_link.SelectedIndex = 0;
+            this._23_link.SelectedIndex = 0;
+
+            this.comboBox1.SelectedIndex = 0;
+        }
+
+
+        public string result = string.Empty;
+        private void logoConfigurator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void parseForEdit(string entry)
+        {
+            try
+            {
+                string[] split = System.Text.RegularExpressions.Regex.Split(entry, localLogos.condSplit);
+                if (split[0].Contains("<") || System.IO.Path.IsPathRooted(split[0]))
+                    this.textBox1.Text = split[0];
+                else this.textBox1.Text = appPath + "\\" + split[0];
+
+                this.cond1_what.Text = split[1];
+                this.cond1_type.SelectedItem = split[2];
+                this.cond1_cond.Text = split[3];
+                this._12_link.SelectedItem = split[4];
+
+                this.cond2_what.Text = split[5];
+                this.cond2_type.SelectedItem = split[6];
+                this.cond2_cond.Text = split[7];
+                this._23_link.SelectedItem = split[8];
+
+                this.cond3_what.Text = split[9];
+                this.cond3_type.SelectedItem = split[10];
+                this.cond3_cond.Text = split[11];
+
+                if (textBox1.Text.Contains(">") && textBox1.Text.Contains("<"))
+                    textBox1.ReadOnly = false;
+                // dyn filename quick check
+
+                if (entry.Contains(localLogos.RestrictionSeriesList)) this.comboBox1.SelectedIndex = 1;
+                else if (entry.Contains(localLogos.RestrictionSeasonList)) this.comboBox1.SelectedIndex = 2;
+                else if (entry.Contains(localLogos.RestrictionEpisodeList)) this.comboBox1.SelectedIndex = 3;
+                else if (entry.Contains(localLogos.RestrictionGroupList)) this.comboBox1.SelectedIndex = 4;
+                else this.comboBox1.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was a problem parsing the Rule: " + ex.Message);
+            }
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.InitialDirectory = appPath;
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+                this.textBox1.Text = openFileDialog1.FileName;
+            this.textBox1.ReadOnly = true;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            this.result = getResult(true);
+            if (result == null)
+            {
+                MessageBox.Show("There was a problem saving the LogoRule.\nCheck that the file exists and try again.");
+            }
+            else
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        string getResult(bool checkFileExists)
+        {
+            if (!this.cond2_what.Enabled || (cond2_what.Text.Trim().Length == 0 && cond2_cond.Text.Trim().Length == 0))
+            {
+                _12_link.SelectedIndex = 0; // force back to AND
+                cond2_what.Text = "";
+                cond2_cond.Text = "";
+                cond2_type.SelectedIndex = 0;
+            }
+            if (!this.cond3_what.Enabled || (cond3_what.Text.Trim().Length == 0 && cond3_cond.Text.Trim().Length == 0))
+            {
+                _23_link.SelectedIndex = 0; // force back to AND
+                cond3_what.Text = "";
+                cond3_cond.Text = "";
+                cond3_type.SelectedIndex = 0;
+            }
+            string file = this.textBox1.Text;
+
+            if (file.Contains(appPath)) file = file.Replace(appPath, "").Remove(0,1); // first \ also away
+
+            if (!checkFileExists || (System.IO.File.Exists(this.textBox1.Text) || (textBox1.Text.Contains(">") && textBox1.Text.Contains("<"))))
+            {
+                return file + localLogos.condSplit
+                            + this.cond1_what.Text + localLogos.condSplit
+                            + this.cond1_type.SelectedItem.ToString() + localLogos.condSplit
+                            + this.cond1_cond.Text + localLogos.condSplit
+                            + this._12_link.SelectedItem.ToString() + localLogos.condSplit
+                            + this.cond2_what.Text + localLogos.condSplit
+                            + this.cond2_type.SelectedItem.ToString() + localLogos.condSplit
+                            + this.cond2_cond.Text + localLogos.condSplit
+                            + this._23_link.SelectedItem.ToString() + localLogos.condSplit
+                            + this.cond3_what.Text + localLogos.condSplit
+                            + this.cond3_type.SelectedItem.ToString() + localLogos.condSplit
+                            + this.cond3_cond.Text + localLogos.condSplit
+                            + this.getRestriction();
+            }
+            else return null;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void cond1_what_TextChanged(object sender, EventArgs e)
+        {
+            validateTxtBox(ref this.cond1_what);
+            enabled2();
+        }
+
+        private void cond2_what_TextChanged(object sender, EventArgs e)
+        {
+            validateTxtBox(ref this.cond2_what);
+            enabled3();
+        }
+
+        private void cond3_what_TextChanged(object sender, EventArgs e)
+        {
+            validateTxtBox(ref this.cond3_what);
+        }
+
+        private void enabled2()
+        {
+            bool enable = true;
+            if (this.cond1_what.Text.Length > 0 && this.cond1_cond.Text.Length > 0)
+                enable = true;
+            else
+                enable = false;
+            this._12_link.Enabled = enable;
+            this.cond2_cond.Enabled = enable;
+            this.cond2_type.Enabled = enable;
+            this.cond2_what.Enabled = enable;
+        }
+
+        private void enabled3()
+        {
+            bool enable = true;
+            if (this.cond2_what.Text.Length > 0 && this.cond2_cond.Text.Length > 0)
+                enable = true;
+            else
+                enable = false;
+            this._23_link.Enabled = enable;
+            this.cond3_cond.Enabled = enable;
+            this.cond3_type.Enabled = enable;
+            this.cond3_what.Enabled = enable;
+        }
+
+        private void cond1_cond_TextChanged(object sender, EventArgs e)
+        {
+            enabled2();
+        }
+
+        private void cond2_cond_TextChanged(object sender, EventArgs e)
+        {
+            enabled3();
+        }
+
+        private void btnDynFilename_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = appPath;
+            if (DialogResult.OK == folderBrowserDialog1.ShowDialog())
+            {
+                this.textBox1.Text = folderBrowserDialog1.SelectedPath + @"\";
+                this.textBox1.ReadOnly = false;
+                this.textBox1.Focus();
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox1.Enabled)
+            {
+            }
+        }
+
+        private string getRestriction()
+        {
+            string restriction = string.Empty;
+            switch (this.comboBox1.SelectedIndex)
+            {
+                case 1:
+                    restriction = localLogos.RestrictionSeriesList;
+                    break;
+                case 2:
+                    restriction = localLogos.RestrictionSeasonList;
+                    break;
+                case 3:
+                    restriction = localLogos.RestrictionEpisodeList;
+                    break;
+                case 4:
+                    restriction = localLogos.RestrictionGroupList;
+                    break;
+
+            }
+            return restriction;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string pasted = Clipboard.GetText();
+            try
+            {
+                parseForEdit(pasted);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There was a problem pasting the logo rule!");
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+
+            string copied = getResult(false);
+            if (copied == null)
+            {
+                MessageBox.Show("There was a problem copying the logo rule!"); 
+            }
+            else
+            {
+                Clipboard.SetText(copied);
+                MessageBox.Show("Logo Rule Configuration copied to Clipboard!"); 
+            }
+        }
+    }
+}
---
+++ tags/v3.2.0/MP-TVSeries/Online Parsing Classes/GetActors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace WindowPlugins.GUITVSeries.Online_Parsing_Classes
+{
+    class GetActors
+    {
+        public List<DBActor> Actors
+        {
+            get { return _actors; }
+        }
+        List<DBActor> _actors = new List<DBActor>();
+
+        public GetActors(int seriesID)
+        {
+            // check if local
+            _actors = DBActor.GetAll(seriesID);
+
+            // success
+            if (_actors.Count != 0) return;
+
+            // get cached actors or download
+            XmlNode node = OnlineAPI.GetActorsList(seriesID);
+            if (node == null) return;
+
+            // add actors to database
+            foreach (XmlNode actorNode in node.SelectNodes("/Actors/Actor"))
+            {
+                DBActor actor = new DBActor();
+
+                actor[DBActor.cSeriesID] = seriesID;
+
+                foreach (XmlNode propertyNode in actorNode)
+                {
+                    actor[propertyNode.Name] = propertyNode.InnerText;
+                }
+                actor.Commit();
+                _actors.Add(actor);
+            }
+
+        }
+    }
+
+}
