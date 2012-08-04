fcd7eb54862da6d85bb6063b941c6c2668b262ff
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2012-08-04 10:43:19 +0000

    Created tag for v3.3.2 release.

---
+++ tags/v3.3.2/MP-TVSeries/DataBase/DBValue.cs
@@ -0,0 +1,180 @@
+#region GNU license
+// MP-TVSeries - Plugin for Mediaportal
+// http://www.team-mediaportal.com
+// Copyright (C) 2006-2007
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#endregion
+
+using System;
+
+namespace WindowPlugins.GUITVSeries
+{
+    public class DBValue
+    {
+        static readonly System.Globalization.NumberFormatInfo provider = new System.Globalization.NumberFormatInfo();
+        static DBValue() { provider.NumberDecimalSeparator = "."; }
+        private String value = String.Empty;
+
+        public override String ToString()
+        {
+            return value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return this == obj as DBValue;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            return base.GetHashCode();
+        }
+
+        public DBValue(String value)
+        {
+            if (value != null)
+                this.value = value;
+            else
+                this.value = String.Empty;
+        }
+
+        public DBValue(Boolean value)
+        {
+            // booleans are 0/1
+            this.value = Convert.ToInt16(value).ToString();
+        }
+
+        public DBValue(int value)
+        {
+            this.value = value.ToString();
+        }
+
+        public DBValue(long value)
+        {
+            this.value = value.ToString();
+        }
+
+        static public implicit operator String(DBValue value)
+        {
+            if (value == null)
+                return "";
+
+            return value.value;
+        }
+
+        static public implicit operator Boolean(DBValue value)
+        {
+            if (value == null)
+                return false;
+
+            if (value.value.Length > 0 && value.value != "0")
+                return true;
+            else
+                return false;
+        }
+        
+        static public implicit operator int(DBValue value)
+        {
+            if (null == value) return 0;
+            try { return Convert.ToInt32(value.value); }
+            catch (System.FormatException) { return 0; }
+        }
+
+        static public implicit operator long(DBValue value)
+        {
+            if (null == value) return 0;
+            try { return Convert.ToInt64(value.value); }
+            catch (System.FormatException) { return 0; }
+        }
+
+        /// <summary>
+        /// NumberDecimalSeperator needs to be "."
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        static public implicit operator double(DBValue value)
+        {
+            if (null == value) return 0;
+            try { return Convert.ToDouble(value.value, provider); }
+            catch (System.FormatException) { return 0; }
+        }
+
+        static public implicit operator DBValue(String value)
+        {
+            return new DBValue(value);
+        }
+
+        static public implicit operator DBValue(Boolean value)
+        {
+            return new DBValue(value);
+        }
+
+        static public implicit operator DBValue(int value)
+        {
+            return new DBValue(value);
+        }
+
+        static public implicit operator DBValue(long value)
+        {
+            return new DBValue(value);
+        }
+
+        static public implicit operator DBValue(double value)
+        {
+            return new DBValue(value.ToString(provider));
+        }
+
+        static public bool operator == (DBValue first, DBValue second)
+        {
+            if ((object)first == null || (object)second == null)
+            {
+                if ((object)first == null && (object)second == null)
+                    return true;
+                else
+                    return false;
+            }
+            return first.value == second.value;
+        }
+
+        static public bool operator != (DBValue first, DBValue second)
+        {
+            if ((object)first == null || (object)second == null)
+            {
+                if ((object)first == null && (object)second == null)
+                    return false;
+                else
+                    return true;
+            } return first.value != second.value;
+        }
+        
+    };
+}
---
+++ tags/v3.3.2/MP-TVSeries/Online Parsing Classes/UpdateSeries.cs
@@ -0,0 +1,176 @@
+#region GNU license
+// MP-TVSeries - Plugin for Mediaportal
+// http://www.team-mediaportal.com
+// Copyright (C) 2006-2007
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#endregion
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Xml;
+using System.Linq;
+
+namespace WindowPlugins.GUITVSeries
+{
+    class UpdateSeries
+    {
+        private List<String> sSeriesIDs = null;
+        private long m_nServerTimeStamp = 0;
+        private List<DBOnlineSeries> listSeries = new List<DBOnlineSeries>();
+        private List<int> listIncorrectIDs = new List<int>();
+
+        public long ServerTimeStamp
+        {
+            get { return m_nServerTimeStamp; }
+        }
+
+        public List<DBOnlineSeries> Results
+        {
+            get
+            {
+                if (listSeries == null)
+                    listSeries = new List<DBOnlineSeries>(ResultsLazy);
+                return listSeries;
+            }
+        }
+
+        /// <summary>
+        /// Lazily Evaluates
+        /// </summary>
+        public IEnumerable<DBOnlineSeries> ResultsLazy
+        {
+            get
+            {
+                foreach (string id in sSeriesIDs)
+                {
+                    var results = Work(id);
+                    foreach (var r in results)
+                        if(r != null && r[DBOnlineSeries.cID] > 0)
+                          yield return r;
+                }
+            }
+        }
+
+        public List<int> BadIds
+        {
+            get { return listIncorrectIDs; }
+        }
+
+        public UpdateSeries(String sSeriesIDs)
+        {
+            listSeries = Work(sSeriesIDs).ToList();
+        }
+
+        public UpdateSeries(List<String> sSeriesIDs)
+        {
+            this.sSeriesIDs = sSeriesIDs;            
+        }
+
+        public UpdateSeries(String sSeriesIDs, String languageID)
+        {
+            listSeries = Work(sSeriesIDs, languageID).ToList();
+        }
+
+        private IEnumerable<DBOnlineSeries> Work(String sSeriesID)
+        {
+            return Work(sSeriesID, "");
+        }
+
+        private IEnumerable<DBOnlineSeries> Work(String sSeriesID, String languageID)
+        {
+            if (sSeriesID.Length > 0)
+            {
+                int result;
+                if (int.TryParse(sSeriesID,out result))
+                    MPTVSeriesLog.Write(string.Format("Retrieving updated Metadata for series {0}",Helper.getCorrespondingSeries(result)), MPTVSeriesLog.LogLevel.Debug);
+
+                XmlNode node = null;
+                if (String.IsNullOrEmpty(languageID))
+                {
+                    node = Online_Parsing_Classes.OnlineAPI.UpdateSeries(sSeriesID);
+                }
+                else
+                {
+                    node = Online_Parsing_Classes.OnlineAPI.UpdateSeries(sSeriesID, languageID);
+                }
+
+                if (node != null)
+                {
+                    foreach (XmlNode itemNode in node.SelectNodes("/Data"))
+                    {
+                        bool hasDVDOrdering = false;
+                        bool hasAbsoluteOrdering = false;
+                        DBOnlineSeries series = new DBOnlineSeries();
+                        foreach (XmlNode seriesNode in itemNode)
+                        {
+                            // first return item SHOULD ALWAYS be the series
+                            if (seriesNode.Name.Equals("Series", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                foreach (XmlNode propertyNode in seriesNode.ChildNodes)
+                                {
+                                    if (propertyNode.Name == "Language") // work around inconsistancy (language = Language)
+                                    {
+                                        series["language"] = propertyNode.InnerText;
+                                    }
+                                    else if (DBOnlineSeries.s_OnlineToFieldMap.ContainsKey(propertyNode.Name))
+                                        series[DBOnlineSeries.s_OnlineToFieldMap[propertyNode.Name]] = propertyNode.InnerText;
+                                    else
+                                    {
+                                        // we don't know that field, add it to the series table
+                                        series.AddColumn(propertyNode.Name, new DBField(DBField.cTypeString));
+                                        series[propertyNode.Name] = propertyNode.InnerText;
+                                    }
+                                }
+                                if (series != null) listSeries.Add(series);
+                            }
+                            else if(!hasDVDOrdering || !hasAbsoluteOrdering || seriesNode.Name.Equals("Episode", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                foreach (XmlNode propertyNode in seriesNode.ChildNodes)
+                                {
+                                    switch (propertyNode.Name)
+                                    {
+                                        case "DVD_episodenumber":
+                                        case "DVD_season":
+                                            if(!String.IsNullOrEmpty(propertyNode.InnerText)) hasDVDOrdering = true;
+                                            break;
+                                        case "absolute_number":
+                                            if (!String.IsNullOrEmpty(propertyNode.InnerText)) hasAbsoluteOrdering = true;
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                        if ((hasAbsoluteOrdering || hasDVDOrdering))
+                        {
+                            string ordering = series[DBOnlineSeries.cEpisodeOrders] == string.Empty ? "Aired|" : (string)series[DBOnlineSeries.cEpisodeOrders];
+                            if (hasAbsoluteOrdering) ordering += "Absolute|";
+                            if (hasDVDOrdering) ordering += "DVD";
+                            series[DBOnlineSeries.cEpisodeOrders] = ordering;
+                        }
+                        if(series != null) yield return series;
+                    }
+                }
+            }
+        }
+    }
+}
+
