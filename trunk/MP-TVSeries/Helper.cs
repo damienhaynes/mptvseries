351e65bcf3cc6605dd061fdd6868837b83ae4f36
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2013-01-18 09:05:18 +0000

    Replace common high order unicode characters before inserting into database, avoids fall-back to sprites in mediaportal's skin engine.
    Note: I only added most common characters which no body will notice or care about when replaced with low order equivalents...everything else we will just have to live with.

--- trunk/MP-TVSeries/Helper.cs
+++ trunk/MP-TVSeries/Helper.cs
@@ -1,4 +1,4 @@
-#region GNU license
+﻿#region GNU license
 // MP-TVSeries - Plugin for Mediaportal
 // http://www.team-mediaportal.com
 // Copyright (C) 2006-2007
@@ -32,6 +32,7 @@ using MediaPortal.Ripper;
 using System.Globalization;
 using System.Reflection;
 using System.Security.Cryptography;
+using System.Linq;
 using System.Net;
 using System.IO;
 using System.Xml;
@@ -90,7 +91,28 @@ namespace WindowPlugins.GUITVSeries
 
             return new string(newLenghtChars);
         }
-        
+
+        public static string RemapHighOrderChars(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return string.Empty;
+
+            // hack to remap high order unicode characters with a low order equivalents
+            // for now, this allows better usage of clipping. This can be removed, once the skin engine can properly render unicode without falling back to sprites
+            // as unicode is more widely used, this will hit us more with existing font rendering only allowing cached font textures with clipping
+          
+            input = input.Replace((char)8211, '-');  //	–
+            input = input.Replace((char)8212, '-');  //	—
+            input = input.Replace((char)8216, '\''); //	‘
+            input = input.Replace((char)8217, '\''); //	’
+            input = input.Replace((char)8220, '"');  //	“
+            input = input.Replace((char)8221, '"');  //	”
+            input = input.Replace((char)8223, '"');  // ‟
+            input = input.Replace((char)8226, '*');  //	•
+            input = input.Replace(((char)8230).ToString(), "...");  //	…
+
+            return input;
+        }
+
         /// <summary>
         /// TitleCases a string
         /// </summary>
