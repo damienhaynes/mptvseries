3fe70575fa7e2c54b1d33533c97f7a052b7cbab4
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2013-03-03 09:44:39 +0000

    Created tag for v3.4.2 release.

---
+++ tags/v3.4.2/MP-TVSeries/Online Parsing Classes/GetEpisodes.cs
@@ -0,0 +1,73 @@
+#region GNU license
+// MP-TVSeries - Plugin for Mediaportal
+// http://www.team-mediaportal.com
+// Copyright (C) 2006-2007
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#endregion
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Xml;
+
+namespace WindowPlugins.GUITVSeries
+{
+    public class GetEpisodes
+    {
+        private List<DBOnlineEpisode> listEpisodes = new List<DBOnlineEpisode>();
+
+        public List<DBOnlineEpisode> Results
+        {
+            get { return listEpisodes; }
+        }
+
+        public GetEpisodes(string seriesID)
+        {
+            int s = 0;
+            if(Int32.TryParse(seriesID, out s) && s > 0)
+                doWork(s);
+        }        
+        
+        public void doWork(int nSeriesID)
+        {            
+            XmlNode node = Online_Parsing_Classes.OnlineAPI.UpdateEpisodes(nSeriesID);
+            if (node == null) return;
+            
+            foreach (XmlNode episodeNode in node.SelectNodes("Episode"))
+            {
+                DBOnlineEpisode episode = new DBOnlineEpisode();
+                foreach (XmlNode propertyNode in episodeNode.ChildNodes)
+                {
+                    if (DBOnlineEpisode.s_OnlineToFieldMap.ContainsKey(propertyNode.Name))
+                        episode[DBOnlineEpisode.s_OnlineToFieldMap[propertyNode.Name]] = propertyNode.InnerText;
+                    else
+                    {
+                        // we don't know that field, add it to the table
+                        episode.AddColumn(propertyNode.Name, new DBField(DBField.cTypeString));
+                        episode[propertyNode.Name] = propertyNode.InnerText;
+                    }
+                }                            
+                listEpisodes.Add(episode);
+            }                             
+            
+        }
+    }
+}
