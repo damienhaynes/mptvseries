39337b0cdc23b6a7ff423dd3c6068b2f7787e705
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2014-01-23 05:47:05 +0000

    Refactoring ISetupForm implementation into it's own class. Will make it easier to launch configuration externally.

--- MP-TVSeries/TVSeriesPlugin.cs
+++ MP-TVSeries/TVSeriesPlugin.cs
@@ -49,8 +49,8 @@ using TraktPlugin.TraktAPI.DataStructures;
 
 namespace WindowPlugins.GUITVSeries
 {
-    [PluginIcons("WindowPlugins.GUITVSeries.Resources.Images.icon_normal.png", "WindowPlugins.GUITVSeries.Resources.Images.icon_faded.png")]
-    public class TVSeriesPlugin : GUIWindow, ISetupForm, IFeedback
+    
+    public class TVSeriesPlugin : GUIWindow, IFeedback
     {
         #region Constructor
         public TVSeriesPlugin()
@@ -81,77 +81,6 @@ namespace WindowPlugins.GUITVSeries
 
             MPTVSeriesLog.Write(string.Format("MP-TVSeries Version: v{0}", Settings.Version.ToString()));
             MPTVSeriesLog.Write("MP-TVSeries Build Date: " + Settings.BuildDate);
-        }
-        #endregion
-
-        #region ISetupForm Members
-        // Returns the name of the plugin which is shown in the plugin menu
-        public string PluginName()
-        {
-            return "MP-TV Series";
-        }
-
-        // Returns the description of the plugin is shown in the plugin menu
-        public string Description()
-        {
-            return "Plugin used to manage and play television series";
-        }
-
-        // Returns the author of the plugin which is shown in the plugin menu
-        public string Author()
-        {
-            return "MP-TVSeries Team";
-        }
-
-        // show the setup dialog
-        public void ShowPlugin()
-        {
-            ConfigurationForm dialog = new ConfigurationForm();
-            dialog.ShowDialog();
-        }
-
-        // Indicates whether plugin can be enabled/disabled
-        public bool CanEnable()
-        {
-            return true;
-        }
-
-        // get ID of windowplugin belonging to this setup
-        public int GetWindowId()
-        {
-            return 9811;
-        }
-
-        // Indicates if plugin is enabled by default;
-        public bool DefaultEnabled()
-        {
-            return true;
-        }
-
-        // indicates if a plugin has its own setup screen
-        public bool HasSetup()
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// If the plugin should have its own button on the main menu of Media Portal then it
-        /// should return true to this method, otherwise if it should not be on home
-        /// it should return false
-        /// </summary>
-        /// <param name="strButtonText">text the button should have</param>
-        /// <param name="strButtonImage">image for the button, or empty for default</param>
-        /// <param name="strButtonImageFocus">image for the button, or empty for default</param>
-        /// <param name="strPictureImage">subpicture for the button or empty for none</param>
-        /// <returns>true  : plugin needs its own button on home
-        ///          false : plugin does not need its own button on home</returns>
-        public bool GetHome(out string strButtonText, out string strButtonImage, out string strButtonImageFocus, out string strPictureImage)
-        {
-            strButtonText = pluginName;
-            strButtonImage = String.Empty;
-            strButtonImageFocus = String.Empty;
-            strPictureImage = "hover_my tv series.png";
-            return true;
         }
         #endregion
 
