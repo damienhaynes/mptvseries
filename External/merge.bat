a9beec447bfb371d1a186bbef55db3a97c066fa1
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2014-02-20 11:26:17 +0000

    Fixed ILMerge compatibility issues with .Net 4.0.

--- External/merge.bat
+++ External/merge.bat
@@ -1,7 +1,19 @@
 @echo off
 
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+    :: 64-bit
+    set PROGS=%programfiles(x86)%
+    rem pause
+    rem echo Current path is %PROGS%	
+    goto CONT
+:32BIT
+    set PROGS=%ProgramFiles%
+    rem echo Current path is %PROGS%
+    rem pause
+:CONT
+
 md tmp
-ilmerge /out:tmp\MP-TVSeries.dll MP-TVSeries.dll CookComputing.XmlRpcV2.dll FollwitApi.dll /target:dll /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /wildcards
+ilmerge /out:tmp\MP-TVSeries.dll MP-TVSeries.dll CookComputing.XmlRpcV2.dll FollwitApi.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
 rem ilmerge /out:tmp\MP-TVSeries.dll MP-TVSeries.dll CookComputing.XmlRpcV2.dll FollwitApi.dll
 IF EXIST MP-TVSeries_UNMERGED.dll del MP-TVSeries_UNMERGED.dll
 ren MP-TVSeries.dll MP-TVSeries_UNMERGED.dll
