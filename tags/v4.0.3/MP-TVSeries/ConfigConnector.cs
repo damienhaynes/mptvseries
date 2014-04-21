454360a31d8d4b0f54a3c7bcfc009c1de679051b
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2014-04-21 07:22:07 +0000

    Created tag for v4.0.3 release.

---
+++ tags/v4.0.3/MP-TVSeries/ConfigConnector.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaPortal.GUI.Library;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Threading;
+using MediaPortal.Configuration;
+
+namespace WindowPlugins.GUITVSeries
+{
+    [PluginIcons("WindowPlugins.GUITVSeries.Resources.Images.icon_normal.png", "WindowPlugins.GUITVSeries.Resources.Images.icon_faded.png")]
+    public class ConfigConnector : ISetupForm
+    {
+        #region ISetupForm Members
+        // Returns the name of the plugin which is shown in the plugin menu
+        public string PluginName()
+        {
+            return "MP-TV Series";
+        }
+
+        // Returns the description of the plugin is shown in the plugin menu
+        public string Description()
+        {
+            return "Plugin used to manage and play television series";
+        }
+
+        // Returns the author of the plugin which is shown in the plugin menu
+        public string Author()
+        {
+            return "MP-TVSeries Team";
+        }
+
+        // show the setup dialog
+        public void ShowPlugin()
+        {
+            ConfigurationForm dialog = new ConfigurationForm();
+            dialog.ShowDialog();
+        }
+
+        // Indicates whether plugin can be enabled/disabled
+        public bool CanEnable()
+        {
+            return true;
+        }
+
+        // get ID of windowplugin belonging to this setup
+        public int GetWindowId()
+        {
+            return 9811;
+        }
+
+        // Indicates if plugin is enabled by default;
+        public bool DefaultEnabled()
+        {
+            return true;
+        }
+
+        // indicates if a plugin has its own setup screen
+        public bool HasSetup()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// If the plugin should have its own button on the main menu of Media Portal then it
+        /// should return true to this method, otherwise if it should not be on home
+        /// it should return false
+        /// </summary>
+        /// <param name="strButtonText">text the button should have</param>
+        /// <param name="strButtonImage">image for the button, or empty for default</param>
+        /// <param name="strButtonImageFocus">image for the button, or empty for default</param>
+        /// <param name="strPictureImage">subpicture for the button or empty for none</param>
+        /// <returns>true  : plugin needs its own button on home
+        ///          false : plugin does not need its own button on home</returns>
+        public bool GetHome(out string strButtonText, out string strButtonImage, out string strButtonImageFocus, out string strPictureImage)
+        {
+            strButtonText = DBOption.GetOptions(DBOption.cPluginName);
+            strButtonImage = String.Empty;
+            strButtonImageFocus = String.Empty;
+            strPictureImage = "hover_my tv series.png";
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ tags/v4.0.3/MP-TVSeries/Configuration/DeleteDialog.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowPlugins.GUITVSeries.Configuration {
+    partial class DeleteDialog {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.radioDeleteFromDiskAndDatabase = new System.Windows.Forms.RadioButton();
+            this.radioDeleteFromDatabase = new System.Windows.Forms.RadioButton();
+            this.radioDeleteFromDisk = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.radioDeleteSubtitles = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioDeleteFromDiskAndDatabase
+            // 
+            this.radioDeleteFromDiskAndDatabase.AutoSize = true;
+            this.radioDeleteFromDiskAndDatabase.Location = new System.Drawing.Point(25, 72);
+            this.radioDeleteFromDiskAndDatabase.Name = "radioDeleteFromDiskAndDatabase";
+            this.radioDeleteFromDiskAndDatabase.Size = new System.Drawing.Size(173, 17);
+            this.radioDeleteFromDiskAndDatabase.TabIndex = 4;
+            this.radioDeleteFromDiskAndDatabase.TabStop = true;
+            this.radioDeleteFromDiskAndDatabase.Text = "Delete from Disk and Database";
+            this.radioDeleteFromDiskAndDatabase.UseVisualStyleBackColor = true;
+            this.radioDeleteFromDiskAndDatabase.Click += new System.EventHandler(this.radioDeleteFromDiskAndDatabase_Click);
+            // 
+            // radioDeleteFromDatabase
+            // 
+            this.radioDeleteFromDatabase.AutoSize = true;
+            this.radioDeleteFromDatabase.Location = new System.Drawing.Point(25, 26);
+            this.radioDeleteFromDatabase.Name = "radioDeleteFromDatabase";
+            this.radioDeleteFromDatabase.Size = new System.Drawing.Size(128, 17);
+            this.radioDeleteFromDatabase.TabIndex = 2;
+            this.radioDeleteFromDatabase.TabStop = true;
+            this.radioDeleteFromDatabase.Text = "Delete from Database";
+            this.radioDeleteFromDatabase.UseVisualStyleBackColor = true;
+            this.radioDeleteFromDatabase.Click += new System.EventHandler(this.radioDeleteFromDatabase_Click);
+            // 
+            // radioDeleteFromDisk
+            // 
+            this.radioDeleteFromDisk.AutoSize = true;
+            this.radioDeleteFromDisk.Location = new System.Drawing.Point(25, 49);
+            this.radioDeleteFromDisk.Name = "radioDeleteFromDisk";
+            this.radioDeleteFromDisk.Size = new System.Drawing.Size(103, 17);
+            this.radioDeleteFromDisk.TabIndex = 3;
+            this.radioDeleteFromDisk.TabStop = true;
+            this.radioDeleteFromDisk.Text = "Delete from Disk";
+            this.radioDeleteFromDisk.UseVisualStyleBackColor = true;
+            this.radioDeleteFromDisk.Click += new System.EventHandler(this.radioDeleteFromDisk_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioDeleteSubtitles);
+            this.groupBox1.Controls.Add(this.radioDeleteFromDatabase);
+            this.groupBox1.Controls.Add(this.radioDeleteFromDiskAndDatabase);
+            this.groupBox1.Controls.Add(this.radioDeleteFromDisk);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 126);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Choice:";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(192, 145);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 0;
+            this.buttonCancel.Text = "&Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(111, 145);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "&OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // radioDeleteSubtitles
+            // 
+            this.radioDeleteSubtitles.AutoSize = true;
+            this.radioDeleteSubtitles.Location = new System.Drawing.Point(25, 95);
+            this.radioDeleteSubtitles.Name = "radioDeleteSubtitles";
+            this.radioDeleteSubtitles.Size = new System.Drawing.Size(99, 17);
+            this.radioDeleteSubtitles.TabIndex = 5;
+            this.radioDeleteSubtitles.TabStop = true;
+            this.radioDeleteSubtitles.Text = "Delete Subtitles";
+            this.radioDeleteSubtitles.UseVisualStyleBackColor = true;
+            this.radioDeleteSubtitles.Click += new System.EventHandler(this.radioDeleteSubtitles_Click);
+            // 
+            // DeleteDialog
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(279, 173);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DeleteDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Delete";
+            this.TopMost = true;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioDeleteFromDiskAndDatabase;
+        private System.Windows.Forms.RadioButton radioDeleteFromDatabase;
+        private System.Windows.Forms.RadioButton radioDeleteFromDisk;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.RadioButton radioDeleteSubtitles;
+    }
+}
