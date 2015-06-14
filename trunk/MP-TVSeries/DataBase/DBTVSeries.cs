703d76d8295c0b4d7733ea0ab9f06c6a4c5a9276
damien.haynes <damien.haynes@92bfd266-0955-0410-bd60-67d1d2af653c>
2015-06-14 02:11:01 +0000

    Added auto backup and restore for database volume if corrupt.
    If the database integrity is good on startup then its backed up, if it's corrupt the corrupted database is backed up and the last known good one is restored automatically.

--- trunk/MP-TVSeries/DataBase/DBTVSeries.cs
+++ trunk/MP-TVSeries/DataBase/DBTVSeries.cs
@@ -1,7 +1,7 @@
 #region GNU license
 // MP-TVSeries - Plugin for Mediaportal
 // http://www.team-mediaportal.com
-// Copyright (C) 2006-2007
+// Copyright (C) 2006-2015
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -22,18 +22,19 @@
 #endregion
 
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using SQLite.NET;
+using MediaPortal.Configuration;
 
 namespace WindowPlugins.GUITVSeries
 {
     /// <summary>
     /// DB class - static, one instance only. 
-    /// holds the SQLLite object and the log object.
+    /// holds the SQLite object and the log object.
     /// </summary>
     public class DBTVSeries
     {
-
         #region static imports
 
         [DllImport("shlwapi.dll")]
@@ -41,13 +42,15 @@ namespace WindowPlugins.GUITVSeries
 
         #endregion
 
-        #region private & init stuff
+        #region privates
         private static SQLiteClient m_db = null;        
 
         private static bool m_bIndexOnlineEpisodes;
         private static bool m_bIndexLocalEpisodes;
         private static bool m_bIndexLocalSeries;
+        #endregion
 
+        #region init
         private static void InitDB()
         {
             if (System.Diagnostics.Process.GetCurrentProcess().ProcessName.ToLower() == "devenv")
@@ -55,30 +58,114 @@ namespace WindowPlugins.GUITVSeries
 
             string databaseFile = Settings.GetPath(Settings.Path.database);
 
-            bool writeToLog = false;
-            if (System.IO.File.Exists(databaseFile)) writeToLog = true;
+            bool databaseExists = true;
+            if (!File.Exists(databaseFile))
+                databaseExists = false;
 
             try
             {
                 m_db = new SQLiteClient(databaseFile);
 
-                m_db.Execute("PRAGMA cache_size=5000;");        // Each page uses about 1.5K of memory
-                m_db.Execute("PRAGMA synchronous='OFF';");
-                m_db.Execute("PRAGMA count_changes=1;");
-                m_db.Execute("PRAGMA full_column_names=0;");
-                m_db.Execute("PRAGMA short_column_names=0;");
-                m_db.Execute("PRAGMA temp_store = MEMORY;");
+                // check database integrity
+                if (databaseExists)
+                {
+                    if (CheckIntegrity())
+                    {
+                        // backup volume if all is good
+                        Backup();
+                    }
+                    else
+                    {
+                        // restore last known good volume if corrupt
+                        Restore();
+                    }
+                }
+                
+                ExecutePragmas();
 
-                if (writeToLog)
-                    MPTVSeriesLog.Write("Successfully opened database '" + databaseFile + "'");
+                MPTVSeriesLog.Write("Successfully opened database. Filename = '{0}'", databaseFile);
+
+                return;
             }
             catch (Exception ex)
             {
-                MPTVSeriesLog.Write("Failed to open database '" + databaseFile + "' (" + ex.Message + ")");
+                MPTVSeriesLog.Write("Failed to open database. Filename = '{0}', Reason = '{1}'", databaseFile, ex.Message);
+
+                // restore last known good volume
+                Restore();
+                ExecutePragmas();
+            }
+        }
+
+        private static void ExecutePragmas()
+        {
+            // Each page uses about 1.5K of memory
+            m_db.Execute("PRAGMA cache_size=5000;");
+            m_db.Execute("PRAGMA synchronous='OFF';");
+            m_db.Execute("PRAGMA count_changes=1;");
+            m_db.Execute("PRAGMA full_column_names=0;");
+            m_db.Execute("PRAGMA short_column_names=0;");
+            m_db.Execute("PRAGMA temp_store = MEMORY;");
+        }
+
+        private static void Backup()
+        {
+            MPTVSeriesLog.Write("Backing up database");
+            
+            string backupDirectory = Config.GetSubFolder(Config.Dir.Database, "MP-TVSeries_Backup");
+            string sourceFile = Settings.GetPath(Settings.Path.database);
+            string destinationFile = Path.Combine(backupDirectory, "TVSeriesDatabase4.db3");
+
+            try
+            {
+                if (!Directory.Exists(backupDirectory))
+                {
+                    Directory.CreateDirectory(backupDirectory);
+                }
+
+                File.Copy(sourceFile, destinationFile, true);
+            }
+            catch (Exception ex)
+            {
+                MPTVSeriesLog.Write("Failed to backup database. Source File = '{0}', Destination File = '{1}', Reason = '{2}'", sourceFile, destinationFile, ex.Message);
+            }
+        }
+
+        private static void Restore()
+        {
+            // back the corrupt database in case user wants to fix themselves
+            string backupDirectory = Config.GetSubFolder(Config.Dir.Database, "MP-TVSeries_Backup");
+            string sourceFile = Settings.GetPath(Settings.Path.database);
+            string destinationFile = Path.Combine(backupDirectory, string.Format("TVSeriesDatabase4-Corrupt-{0}.db3", DateTime.Now.ToString("yyyyMMddHHmmss")));
+            
+            MPTVSeriesLog.Write("Backing up corrupt database. Filename = '{0}'", destinationFile);
+
+            try
+            {
+                File.Copy(sourceFile, destinationFile, true);
+            }
+            catch (Exception ex)
+            {
+                MPTVSeriesLog.Write("Failed to backup corrupt database. Source File = '{0}', Destination File = '{1}', Reason = '{2}'", sourceFile, destinationFile, ex.Message);
+            }
+
+            MPTVSeriesLog.Write("Restoring last known good database");
+
+            sourceFile = Path.Combine(backupDirectory, "TVSeriesDatabase4.db3");
+            destinationFile = Settings.GetPath(Settings.Path.database);
+            
+            try
+            {
+                File.Copy(sourceFile, destinationFile, true);
+            }
+            catch (Exception ex)
+            {
+                MPTVSeriesLog.Write("Failed to restore database. Source File = '{0}', Destination File = '{1}', Reason = '{2}'", sourceFile, destinationFile, ex.Message);
             }
         }
         #endregion
-        
+
+        #region public methods
         public static void CreateDBIndices(string sCommand, string sTable, bool bSetFlag)
         {            
             try
@@ -119,16 +206,16 @@ namespace WindowPlugins.GUITVSeries
             {            
                 m_db.Close();
                 m_db.Dispose();
-                m_db = null; ;
-                MPTVSeriesLog.Write("Successfully closed Database: " + databaseFile);
+                m_db = null;
+                MPTVSeriesLog.Write("Successfully closed database. Filename = '{0}'", databaseFile);
             }
             catch (Exception)
             {
-                MPTVSeriesLog.Write("Failed closing Database: " + databaseFile);
+                MPTVSeriesLog.Write("Failed to close database. Filename = '{0}'", databaseFile);
             }
         }
 
-        public static SQLiteResultSet Execute(String sCommand)
+        public static SQLiteResultSet Execute(String query)
         {
             if (m_db == null)
             {
@@ -138,20 +225,45 @@ namespace WindowPlugins.GUITVSeries
             SQLite.NET.SQLiteResultSet result;
             try
             {
-                MPTVSeriesLog.Write("Executing SQL: ", sCommand, MPTVSeriesLog.LogLevel.DebugSQL);
-                result = m_db.Execute(sCommand);
-                MPTVSeriesLog.Write("Success, returned Rows: ", result.Rows.Count, MPTVSeriesLog.LogLevel.DebugSQL);
+                MPTVSeriesLog.Write(string.Format("Executing SQL query. Query = '{0}'", query), MPTVSeriesLog.LogLevel.DebugSQL);
+                result = m_db.Execute(query);
+                MPTVSeriesLog.Write(string.Format("Successfully executed SQL query. Row Count = '{0}'", result.Rows.Count), MPTVSeriesLog.LogLevel.DebugSQL);
                 return result;
             }
             catch (Exception ex)
             {
-                MPTVSeriesLog.Write("SQL Execution Failed, Reason = '{0}', SQL = '{1}'", ex.Message, sCommand);
+                MPTVSeriesLog.Write("SQL Execution Failed. Reason = '{0}', Query = '{1}'", ex.Message, query);
                 return new SQLiteResultSet();
             }
         }
 
-        #region public properties
+        public static bool CheckIntegrity()
+        {
+            string query = "PRAGMA integrity_check;";
+            MPTVSeriesLog.Write("Executing SQL integrity check");
 
+            SQLiteResultSet results = Execute(query);
+            if (results != null)
+            {
+                if (results.Rows.Count == 1)
+                {
+                    SQLiteResultSet.Row arr = results.Rows[0];
+                    if (arr.fields.Count == 1)
+                    {
+                        if (arr.fields[0] == "ok")
+                        {
+                            MPTVSeriesLog.Write("Database integrity check succeeded");
+                            return true;
+                        }
+                    }
+                }
+            }
+            MPTVSeriesLog.Write("Integrity check failed, database is corrupt. Filename = '{0}'", m_db.DatabaseName);
+            return false;
+        }
+        #endregion
+
+        #region public properties
         public static bool IsDatabaseOnNetworkPath
         {
             get
@@ -160,7 +272,6 @@ namespace WindowPlugins.GUITVSeries
                 return PathIsNetworkPath(databaseFile);
             }
         }
-
         #endregion
     };
 }
