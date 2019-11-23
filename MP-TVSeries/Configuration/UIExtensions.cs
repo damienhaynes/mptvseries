b397f880b467b6b3b0ad32c64a6d2b0121d96c24
damienhaynes <damien.haynes@gmail.com>
2019-11-23 14:20:41 +1100

    Missing file on last commit.

---
+++ MP-TVSeries/Configuration/UIExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace WindowPlugins.GUITVSeries.Configuration
+{
+  public static class UIExtensions
+  {
+    public static void DoubleBuffered( this DataGridView dgv, bool setting )
+    {
+      Type dgvType = dgv.GetType();
+      PropertyInfo pi = dgvType.GetProperty( "DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic );
+      pi.SetValue( dgv, setting, null );
+    }
+  }
+}
