d64353dd1dc23ab5624a7928683c6d29a74ef45f
damienhaynes <damien.haynes@gmail.com>
2019-12-03 11:25:44 +1100

    Align versioning of Config launcher with MP-TVSeries library. Also updated MPEI package.

--- ConfigLauncher/Properties/AssemblyInfo.cs
+++ ConfigLauncher/Properties/AssemblyInfo.cs
@@ -1,26 +1,25 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MP-TVSeries Configuraiton Launcher")]
-[assembly: AssemblyDescription("MP-TVSeries Configuraiton Launcher")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MP-TVSeries Configuraiton Launcher")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "MP-TVSeries Configuraiton Launcher" )]
+[assembly: AssemblyDescription( "MP-TVSeries Configuraiton Launcher" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "MP-TVSeries Configuraiton Launcher" )]
+[assembly: AssemblyCopyright( "Copyright © 2019" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a8955056-78bf-4390-9c3a-b3ac6b5072e6")]
+[assembly: Guid( "a8955056-78bf-4390-9c3a-b3ac6b5072e6" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +28,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.2042")]
-[assembly: AssemblyFileVersion("1.0.3.2042")]
+// Align version with main library
+[assembly: AssemblyVersion( "4.3.3.6" )]
+[assembly: AssemblyFileVersion( "4.3.3.6" )]
